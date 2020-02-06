a7eeb8838404f2e344833d8ed41df47ff662c5cf
victor <josevictor.leon@suramexico.com>
2020-02-06 13:20:15 -0600

    add routes for home, and add header component

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -3,15 +3,16 @@ import './App.css';
 import {
   BrowserRouter as Router,
   Route
-} from "react-router-dom";
+} from 'react-router-dom';
 import store from './store'
 import { Provider } from 'react-redux';
-
-const Home = () => <div>Home</div>
+import { Home } from "./containers";
+import { Head } from './components/'
 
 const App = () => {
   return (
     <Provider store={store}>
+      <Head />
       <Router>
         <Route exact path="/" component={Home} />
         <Route component={Home} path="/pokemon/:name" />
