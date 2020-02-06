857f3c2f676d1dc60a1b2905ea03ff84f246a530
victor <josevictor.leon@suramexico.com>
2020-02-06 13:29:37 -0600

    connect home with redux and retrieve data for print pokemons

--- pokedex/src/containers/Home/index.js
+++ pokedex/src/containers/Home/index.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { getPokedex } from '../../actions/getData'
 import {
   MainContainer,
   Row
@@ -8,15 +10,32 @@ import {
 } from '../../components'
 
 class Home extends Component {
+  componentDidMount () {
+    this.props.getPokedex()
+  }
   render () {
+    const { pokemons } = this.props
     return (
       <MainContainer>
+
         <Row>
-        <Mosaic/>
+        {
+          pokemons && pokemons.length > 0 && <Mosaic pokemons={pokemons} />
+        }
         </Row>
       </MainContainer>
     )
   }
 }
 
-export default Home
+const MapStateToProps = ({ pokemons }) => {
+  return {
+    pokemons
+  }
+}
+
+const MapDispatchToProps = {
+  getPokedex
+}
+
+export default connect(MapStateToProps, MapDispatchToProps)(Home)
