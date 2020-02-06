d70afd4ee5162b53e16de9ac5b573cb0f78be1e5
victor <josevictor.leon@suramexico.com>
2020-02-06 13:36:12 -0600

    add capaility for show pokemons per page in mosaic component

--- pokedex/src/components/Mosaic/index.js
+++ pokedex/src/components/Mosaic/index.js
@@ -1,27 +1,42 @@
-import React from 'react' 
+import React, { useEffect, useState } from 'react' 
 import {
   Row
 } from './styled'
 import { Flip, Button } from '../'
 
 
-const Mosaic = ({ pokemons }) => (
-  <Row>
-    <Row alignCenter>
-      <Button>First</Button>
-      <Button>{'<'}</Button>
-      <div className='page-counter'>Page: 1</div>
-      <Button >{'>'}</Button>
-      <Button>Last</Button>
-    </Row>
+const Mosaic = ({ pokemons }) => {
+  const [temporalPokemons, setTemporalPokemons] = useState([])
+  const [elementPerPage, setElementsPerPage] = useState(8)
+  const [currentPage, setCurrentPage] = useState(0)
+  const [totalPages, setTotalPages]= useState(0)
+  
+  useEffect(() => {
+    let pages = Math.ceil(pokemons.length / elementPerPage)
+    setTotalPages(pages)
+    let newPokemons
+    if (currentPage === 0) newPokemons = pokemons.slice(currentPage, elementPerPage)
+    else newPokemons = pokemons.slice(currentPage, currentPage * elementPerPage)
+    setTemporalPokemons(newPokemons)
+  }, [pokemons])
+  return (
     <Row>
-    {
-      pokemons.map((pokemon, i) => (
-        <Flip key={i} data={pokemon} onClick={() => console.log(this.props)} />
-      ))
-    } 
+      <Row alignCenter>
+        <Button>First</Button>
+        <Button>{'<'}</Button>
+        <div className='page-counter'>Page: 1</div>
+        <Button >{'>'}</Button>
+        <Button>Last</Button>
+      </Row>
+      <Row>
+      {
+        temporalPokemons.map((pokemon, i) => (
+          <Flip key={i} data={pokemon} onClick={() => console.log(this.props)} />
+        ))
+      } 
+      </Row>
     </Row>
-  </Row>
-)
+  )
+}
 
 export default Mosaic
