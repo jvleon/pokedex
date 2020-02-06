f13949473a1822c8257a19169bdaaef262093926
victor <josevictor.leon@suramexico.com>
2020-02-06 13:19:04 -0600

    make head component

---
+++ pokedex/src/components/Head/styled.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const Header = styled.header`
+  width: 100%;
+  height: 150px;
+  display: flex;
+  padding: 30px 5px;
+  box-sizing: border-box;
+  align-items: flex-end;
+  background: linear-gradient(to bottom, #F8FFAE, #43C6AC);
+  & > img {
+    width: 300px;
+  }
+`
