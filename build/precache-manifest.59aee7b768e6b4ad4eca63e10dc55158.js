24cbdc1e12d21d6da265069d4cfb2db821f03d01
victor <josevictor.leon@suramexico.com>
2020-02-06 16:00:15 -0600

    build package

---
+++ build/precache-manifest.59aee7b768e6b4ad4eca63e10dc55158.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abd7cc657de8cbb1fe9ef4af6aeb46cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62d4d9f41142ae0956bf",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "04e8b4987d93dccb2b31",
+    "url": "/static/js/2.fd78c035.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.fd78c035.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62d4d9f41142ae0956bf",
+    "url": "/static/js/main.e3993d1d.chunk.js"
+  },
+  {
+    "revision": "a3ae24c73f7b90477b95",
+    "url": "/static/js/runtime-main.801dd740.js"
+  },
+  {
+    "revision": "023d617a1b733e2862bc8d439cface20",
+    "url": "/static/media/logo.023d617a.png"
+  }
+]);
